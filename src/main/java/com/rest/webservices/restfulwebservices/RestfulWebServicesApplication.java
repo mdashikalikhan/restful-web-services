d9aa3ed5e57b402ef7a48445a6da4e47e3603b44
ashik.khan <ashik.khan@N1084.sonaliintellect.com>
2019-04-18 20:24:58 +0600

    Commit Problem Resolved ...Message Internationalization last part at 18 April, 2019

--- src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -5,15 +5,11 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
+
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
-=======
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
->>>>>>> branch 'master' of https://github.com/mdashikalikhan/restful-web-services.git
+
 
 
 @SpringBootApplication
@@ -28,20 +24,10 @@ public class RestfulWebServicesApplication {
 		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
-<<<<<<< HEAD
+
+	}
 		
-=======
-	}
-	
-	@Bean
-	public ResourceBundleMessageSource bundleMessageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		return messageSource;
->>>>>>> branch 'master' of https://github.com/mdashikalikhan/restful-web-services.git
-	}
-	
-	
+		
 	/**
 	 * The following bean is eliminated by using AcceptHeaderLocaleResolver instead of SessionLocaleResolver class.
 	 * And the benefit is Accept-Language is not needed in controller method. Another advantage is we can configure
